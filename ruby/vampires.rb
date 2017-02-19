71beb634a4dcbe92a4403733e6f98852de9c22af
bmerkle3 <braidymerkle@gmail.com>
2017-02-18 19:35:30 -0600

    satisfies first 4 bullet points of release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,34 @@
+
 puts "What is your name?"
   name = gets.chomp
 
 puts "How old are you?"
-  age = gets.chomp.to_i 
+  age = gets.chomp 
 puts "What year were you born?"
-  birth_year = gets.chomp.to_i
+  birth_year = gets.chomp
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y or n)"
   garlic_bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance? (y or n)"
   health_insurance = gets.chomp
+
+
+if name == "Drake Cula" || name == "Tu Fang"
+   puts "Definitely a vampire."
+ elsif (age.to_i == 2017 - birth_year.to_i) && (garlic_bread == "y" || health_insurance == "y")
+   puts "Probably not a vampire"
+ elsif (age.to_i != 2017 - birth_year.to_i) && ((garlic_bread == "y" && health_insurance == "n") || (garlic_bread == "n" && health_insurance == "y"))
+   puts "Probably a vampire"
+ elsif age.to_i != 2017 - birth_year.to_i && garlic_bread == "n" && health_insurance == "n"
+   puts "Almost certainly a vampire"
+ else 
+  puts "Results inconclusive"
+
+end
+
+
+
+
+
+
