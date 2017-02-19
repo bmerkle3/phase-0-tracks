7849e45edd3e04012412c2313b9244151c2fc15e
bmerkle3 <braidymerkle@gmail.com>
2017-02-18 18:29:09 -0600

    adds variables to save answers

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,13 @@
 puts "What is your name?"
+  name = gets.chomp
 
-puts "How old are you? What year were you born?"
+puts "How old are you?"
+  age = gets.chomp.to_i 
+puts "What year were you born?"
+  birth_year = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y or n)"
+  garlic_bread = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (y or n)"
+  health_insurance = gets.chomp
