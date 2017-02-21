001fbc3fdefb5d2fba5abd82c018079856b0ed44
bmerkle3 <braidymerkle@gmail.com>
2017-02-21 17:36:50 -0600

    prints colors and colors with one letter chopped off if they are over 4 letters long.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,34 +1,73 @@
-def practice_method
-  puts "Running..."
-  status = "Complete."
-  yield(status)
-end
+# def practice_method
+#   puts "Running..."
+#   status = "Complete."
+#   yield(status)
+# end
 
-practice_method { |status| puts "status: #{status}"}
-
-
-actors = [ "Julia Roberts", "Tom Hanks", "Mel Gibson" ]
-  actors.each do |actor|
-    puts actor
-  end
-  actors.map! do |lower_case|
-    puts lower_case.downcase
-end
+# practice_method { |status| puts "status: #{status}"}
 
 
-actors_movies = {
-  "Julia Roberts" => "Pretty Woman", 
-  "Tom Hanks" => "Forrest Gump",
-  "Mel Gibson" => "Lethal Weapon",
-  "Reese Witherspoon" => "Legally Blonde"
-}
+# actors = [ "Julia Roberts", "Tom Hanks", "Mel Gibson" ]
+#   actors.each do |actor|
+#     puts actor
+#   end
+#   actors.map! do |lower_case|
+#     puts lower_case.downcase
+# end
+
+
+# actors_movies = {
+#   "Julia Roberts" => "Pretty Woman", 
+#   "Tom Hanks" => "Forrest Gump",
+#   "Mel Gibson" => "Lethal Weapon",
+#   "Reese Witherspoon" => "Legally Blonde"
+# }
   
-actors_movies.each do |actor, movie|
-  puts "#{actor} was in #{movie}"
+# actors_movies.each do |actor, movie|
+#   puts "#{actor} was in #{movie}"
+# end
+
+# upcase_actors_movies = actors_movies.map do |actor, movie|
+#   movie.upcase
+# end
+
+# p upcase_actors_movies
+
+
+# array1 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+# p array1.delete_if { |array1| 
+#    array1 < 2}
+
+
+# p array1.keep_if { |array1|
+#   array1 > 2 }
+
+# p array1.select {|x|
+#   x > 3  }
+
+
+array2 = [ "orange", "green", "blue", "red", "yellow", "aqua"]
+ 
+ array2.each do |colors|
+    colors.length
+    good_length = 4 
+   if colors.length > good_length
+   p colors.chop
+   while good_length == false
+
+   end
+     good_length = true
+
+  end
+ p 
 end
 
-upcase_actors_movies = actors_movies.map do |actor, movie|
-  movie.upcase
-end
+   # while good_length > 4 
+   # colors.chop
+   
+ # end
+ # end
+ # puts colors
 
-p upcase_actors_movies
+
