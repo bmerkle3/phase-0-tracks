6aa00c1e4db41267b4f7e86d8ec5081e972c85f6
bmerkle3 <braidymerkle@gmail.com>
2017-02-22 16:44:48 -0600

    adds hash_pract.rb

---
+++ ruby/hash_pract.rb
@@ -0,0 +1,20 @@
+post = {
+  "date" => "6/6/10",
+  "title" => "About my Turtle",
+  "author" => "Janet Finkle",
+  "body" => "I just love my turtle Rudolph! Today he moved two inches and sat in my hand for 5 minutes."
+}
+
+p post["author"]
+
+designer_application = {
+  "Name" => "",
+  "Address" => "",
+  "Email" => "",
+  "Phone" => "",
+  "Fave shade of blue:" => "", 
+  "Wallpaper preferences (check any that apply):" => "", 
+  "Ombre is:" => ""
+
+
+}
