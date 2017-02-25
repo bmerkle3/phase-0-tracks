7225eab9f0c39a9137cb357f3fbc6debfa6615ac
bmerkle3 <braidymerkle@gmail.com>
2017-02-24 18:37:33 -0600

    completes release 1

--- ruby/hash_pract.rb
+++ ruby/hash_pract.rb
@@ -8,13 +8,32 @@ post = {
 p post["author"]
 
 designer_application = {
-  "Name" => "",
-  "Address" => "",
-  "Email" => "",
-  "Phone" => "",
-  "Fave shade of blue:" => "", 
-  "Wallpaper preferences (check any that apply):" => "", 
-  "Ombre is:" => ""
-
+  name: "Bob Graham",
+  address: "222 Hu Court",
+  email: "bobgraham@bobgraham.com",
+  phone: "999-655-3333",
+  fave_shade_of_blue: "indigo", 
+  wallpaper_preferences: "Photorealistic woodsy scenes (with or without squirrels)", 
+  ombre_is: "Fierce"
 
 }
+
+p designer_application[:phone]
+
+designer_application[:phone] = "989-655-3333"
+
+p designer_application[:phone]
+
+designer_application[:empl_status] = "employed"
+
+p designer_application.keys
+
+p designer_application
+
+v = [:address]
+
+p designer_application[:v]
+
+p designer_application[:name] + designer_application[:address]
+
+p designer_application
