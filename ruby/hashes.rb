9703f089d75c2a100e09289e7cbe17f8d3d7553c
bmerkle3 <braidymerkle@gmail.com>
2017-02-24 22:38:10 -0600

    adds interface to hash and adds array for additional info (incl interface question)

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,7 +1,8 @@
 # typical responses stored in hash keys
 client_details = {
-  name: "n",
+  name: "",
   age: "",
+  kids: "", 
   number_children: "",
   decor_theme: "",
   fav_color: ""
@@ -9,12 +10,32 @@ client_details = {
 
 
 # 1. interface will be necessary to prompt designer/user for info
-puts "Client Name"
+puts "Client Name:"
+  client_details["name"] = gets.chomp
+puts "Client Age:" 
+  client_details["age"] = gets.chomp
+puts "Does client have children? y or n"
+  client_details["kids"] = gets.chomp
+puts "Number of Children:" 
+  client_details["number_children (if applicable"] = gets.chomp
+puts "Theme of decor:"
+  client_details["decor_theme"] = gets.chomp
+puts "Favorite color of client:"
+  client_details["fav_color"] = gets.chomp
+# array here for additional info - unsure of how many additional pieces of info, not able to add keys.
+additional_info = [""]
 
-n = gets.chomp
 
+puts "Additional client information (enter 'none' when complete)"
+  loop do
+    input = gets.chomp
+    break if input == "none"
+    additional_info << input
+  end
 
+p client_details["name"]
 
+p additional_info
 
 
 
