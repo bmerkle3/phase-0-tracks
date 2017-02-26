7fc81efa697476f1acc3e637508ddd17da14c2ba
bmerkle3 <braidymerkle@gmail.com>
2017-02-25 20:45:08 -0600

    improves spacing of data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,397 +1,262 @@
 school_district = {
   madison_elementary: { 
-      rooms: [
-        "Office"
-        1st_grade: [
+      grades: {
+        
+        1st_grade: {
 
-          teachers: [
-            ms_brown:[ 
+          teachers: {
+            ms_brown:{
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-        ]
-            mr_anderson: [
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+            mr_anderson: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-            
-          ]
-          
-          
-        ]
-        2nd_Grade: [ 
-          teachers: [
-            mr_schmidt: [
-              room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          
-        ]
-      ] 
-            miss_clark: [
+              ]
+            }
+          }
+        }
+        2nd_grade: { 
+          teachers: {
+            mr_schmidt: {
+              room_contents: [
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            } 
+            miss_clark: {
               room_contents:[
                 "table", "chair", "chair", "chair", "chair", "chair", 
                 "table", "chair", "chair", "chair", "chair", "chair",
-                "table", "chair", "chair", "chair", "chair", "chair", 
-                "table", "chair", "chair", "chair", "chair", "chair"
-                "table", "chair", "chair", "chair", "chair", "chair" 
-          
-        ]
-            ]
-            
-          ]
-          
-        3rd_grade: [
-          teachers: [
-            mrs_mcbride: [
+              ]
+            }
+          } 
+        }
+        3rd_grade: {
+          teachers: {
+            mrs_mcbride: {
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+            ms_sunny: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-            ms_sunny: [
+              ]
+            }
+          }
+        }
+        4th_grade: {
+          teachers: {
+            mr_miller:{
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+            mr_shay: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-          ]
-          
-        ]
-        4th_grade: [
-          teachers: [
-            mr_miller:[
+              ]
+            }
+          }
+        }
+        5th_grade: {
+          teachers: {
+            mr_crawford: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-            ]
-            ]
-
-            mr_shay: [
+              ]
+            }
+            mrs_lambert: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-            ]
-            ]
-          ]
-          
-        ]
-        5th_grade: [
-          teachers: [
-            mr_crawford: [
-              room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-
-            mrs_lambert: [
-              room_contents:[
-            "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-          ]
-            
-          
-        ]
-
+              ]
+            }
+          }
+        }
     }
 
   park_elementary: {
-      rooms: [
-        "Office"
-        1st_grade: [
-          teachers: [
-            mrs_melendez: [
+      grades: {
+        1st_grade: {
+          teachers: {
+            mrs_melendez: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-            mr_acevedo: [
+              ]
+            }
+            mr_acevedo: {
+              room_contents:[            
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair",
+              ]
+            }
+          }
+        }
+        2nd_Grade: { 
+          teachers: {
+            mrs_montoya: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-          ]
-          
-        ]
+             ]
+            }
 
-        2nd_Grade: [ 
-          teachers: [
-            mrs_montoya: [
+            mrs_tapia {
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+          }
+        }
+
+        3rd_grade: {
+          teachers: {
+            mr_sullivan: {
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+
+            ms_weaver: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-
-            mrs_tapia [
+              ]
+            }
+          }
+        }
+        4th_grade: {
+          teachers: {
+            mrs_zhang: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-          ]
-          
-          
-        ]
-
-        3rd_grade: [
-
-          teachers: [
-            mr_sullivan: [
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+            mr_rogers: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-
-            ms_weaver: [
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+          }
+        }
+        5th_grade: {
+          teachers:{ 
+            miss_davis: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-          ]
-
-        ]
-        4th_grade: [
-
-          teachers: [
-            mrs_zhang: [
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+            mrs_shepard: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-            mr_rogers: [
-              room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-          ]
-          
-          
-        ]
-        5th_grade: [
-
-          teachers: [ "Miss. Davis"
-          ]
-            room_contents:[
-            "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-          
-        ]
-      ]
+              ]
+            }
+          }
+        }
+      }
   }
   
   butterfield_elementary: {
-      rooms: [
-        "Office"
-        1st_grade: [
-          teachers: [
-            mrs_dunn: [
+      grades: {
+        1st_grade: {
+          teachers: {
+            mrs_dunn: {
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+            ms_galvan: {
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+          } 
+        }
+        
+        2nd_Grade: { 
+          teachers: {
+            mr_chaney: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-            ms_galvan: [
+              ]
+            }
+            ms_prince: {
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+          }
+        }
+
+        3rd_grade: {
+          teachers: {
+            mrs_kane: {
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+            mrs_payne: {
               room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-            ]
-          ]
-          
-          
-        ]
-        
-        2nd_Grade: [ 
-          teachers: [
-          ]
-          room_contents:[
+              ]
+            }
+          }
+        }
+        4th_grade: {
+          teachers: {
+            mr_hogan: {
+              room_contents:[
+            "table", "chair", "chair", "chair", "chair", "chair", 
+            "table", "chair", "chair", "chair", "chair", "chair", 
+              ]
+            }
+            ms_baird: {
+              room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-          
-        ]
-
-        3rd_grade: [
-          teachers: [
-          ]
-          room_contents:[
+              ]
+            }
+          }
+        }
+        5th_grade: {
+          teachers: {
+            mr_powers: {
+              room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-          
-        ]
-        4th_grade: [
-          teachers: [
-          ]
-          room_contents:[
+              ]
+            }
+            mrs_hopkins: {
+              room_contents:[
             "table", "chair", "chair", "chair", "chair", "chair", 
             "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-          
-        ]
-        5th_grade: [
-          teachers: [
-          ]
-            room_contents:[
-            "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-          
-        ]
-      ]
+              ]
+            }
+          } 
+        }
+      }
   }
-  
-  }
-  
-  westlake_elementary: {
-    rooms: [
-        "Office"
-        1st_grade: [
-          teachers: [
-          ]
-          room_contents:[
-            "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-        ]
-        
-        2nd_Grade: [ 
-          teachers: [
-          ]
-          room_contents:[
-            "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-        ]
-
-        3rd_grade: [
-          teachers: [
-          ]
-          room_contents:[
-            "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-        ]
-        4th_grade: [
-          teachers: [
-          ]
-          room_contents:[
-            "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-        ]
-        5th_grade: [
-          teachers: [
-          ]
-            room_contents:[
-            "table", "chair", "chair", "chair", "chair", "chair", 
-            "table", "chair", "chair", "chair", "chair", "chair",
-             "table", "chair", "chair", "chair", "chair", "chair", 
-             "table", "chair", "chair", "chair", "chair", "chair"
-              "table", "chair", "chair", "chair", "chair", "chair" 
-          ]
-        ]
-      ]
-  }
-  
-  }
-  
 }
