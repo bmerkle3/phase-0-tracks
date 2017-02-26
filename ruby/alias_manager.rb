7a0c84b42bb8c90f931d1d1659604da4ae132010
bmerkle3 <braidymerkle@gmail.com>
2017-02-26 17:07:06 -0600

    returns capitalized Gidmodoe Vussit, but then reverses all characters in string

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,21 +2,22 @@
 puts "enter original name:"
 original_name = gets.chomp
 
-# 2. create a vowel data structure containing replacement vowels for new spy name
-#  --> within the next_vowel method
 
-#  pool of consonants to create new spy name
+
+
 
 # . use .downcase method to make all characters lower case
 working_copy_name = original_name.downcase.split('')
-
 # .  use the .split or .chars method to break apart letters
 
-# for each letter, see if the character matches the vowel array
-  # if it does, switch to the next index in the array (#map or #each)
-  # if not, leave the letter alone. 
-  # 
 
+
+
+# 2. create a vowel data structure containing replacement vowels for new spy name
+#  --> within the next_vowel method
+# for each letter, see if the character matches the vowel hash
+  # if it does, look at the next key (#map or #each)
+  # if not, leave the letter alone. 
 
   # collection is an array
 def next_vowel(collection)
@@ -28,8 +29,11 @@ def next_vowel(collection)
   
 end 
  
-p next_vowel(working_copy_name)
+ next_vowel(working_copy_name)
 
+
+
+#  pool of consonants to create new spy name
 def next_consonant(array_version)
   consonant_hash = {b: "c", c: "d", d: "f", f: "g", g: "h", h: "j", j: "k", k: "l", l: "m", m: "n", n: "p", p: "q", q: "r", r: "s", s: "t", t: "v", v: "w", w: "x", x: "y", y: "z", z: "b" }
 
@@ -41,16 +45,31 @@ def next_consonant(array_version)
 
 end
 
-p next_consonant(working_copy_name)
 
-p next_consonant(next_vowel(working_copy_name))
 
+nexted_letters =  next_consonant(next_vowel(working_copy_name)).join
+
+alias_name_as_array = nexted_letters.split.map(&:capitalize).join(' ')
+p alias_name_as_array
+correct_order_alias = alias_name_as_array.reverse.to_s
+
+
+final_alias = correct_order_alias.capitalize!
+
+puts correct_order_alias
+
+
+
+
+# def swap_names(array_of_names)
+#   array.reverse! { |array_of_names| puts array_of_names}
+# end
+ 
+  
 
 
   
 
-# if not, see if it matches consonant array
-  # if it matches in the consonant array, switch to the next index in the array
 
 # if other character like a space or - or something, keep the same.
 # . use a .replace_if method
