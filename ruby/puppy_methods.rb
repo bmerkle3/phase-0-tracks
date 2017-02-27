ee779a33d31e957899d86fed6d1376e776398df6
bmerkle3 <braidymerkle@gmail.com>
2017-02-27 11:47:33 -0600

    uncomments puppy class work

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,55 +1,55 @@
-# class Puppy
+class Puppy
 
-#   def fetch(toy)
-#     puts "I brought back the #{toy}!"
-#     toy
-#   end
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
 
-# end
-
-
-# class Puppy
-#  def initialize
-#     print "In itializing new puppy instance ..."
-#  end
-
-#  def fetch(toy)
-#    puts "I brought back the #{toy}!"
-#    toy
-#  end
-
-#  def speak(num)
-#    puts 'Woof!' * num
-#  end
-
-#  def roll_over
-#    print "*rolls over*" + "\n"
-#  end
-
-#  def dog_years(human_years)
-#    print human_years * 7
-#  end
-
-#  def add_trick(trick)
-#    if trick == 'waits'
-#     puts 'Dog is staying'
-#    elsif trick == 'sit'
-#     puts 'Dog is sitting'
-#   else
-#    puts 'Good dog'
-#  end
-
-#  end
-# end
+end
 
 
-# teddy = Puppy.new
-# teddy.fetch('ball')
-# teddy.speak(4)
-# teddy.roll_over
-# teddy.dog_years(4)
-# teddy.add_trick('bark')
-# teddy.initialize
+class Puppy
+ def initialize
+    print "In itializing new puppy instance ..."
+ end
+
+ def fetch(toy)
+   puts "I brought back the #{toy}!"
+   toy
+ end
+
+ def speak(num)
+   puts 'Woof!' * num
+ end
+
+ def roll_over
+   print "*rolls over*" + "\n"
+ end
+
+ def dog_years(human_years)
+   print human_years * 7
+ end
+
+ def add_trick(trick)
+   if trick == 'waits'
+    puts 'Dog is staying'
+   elsif trick == 'sit'
+    puts 'Dog is sitting'
+  else
+   puts 'Good dog'
+ end
+
+ end
+end
+
+
+teddy = Puppy.new
+teddy.fetch('ball')
+teddy.speak(4)
+teddy.roll_over
+teddy.dog_years(4)
+teddy.add_trick('bark')
+teddy.initialize
 
 
 class Cat
