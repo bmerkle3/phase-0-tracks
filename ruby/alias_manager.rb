ba9bac365ff686296861b83c6153eef9621a84e2
bmerkle3 <braidymerkle@gmail.com>
2017-02-26 20:14:34 -0600

    adds loop for release 1

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,81 +1,68 @@
-# 1. allows user to enter a name and have it stored as a variable temporarily
-puts "enter original name:"
-original_name = gets.chomp
 
-
-
-
-
-# . use .downcase method to make all characters lower case
-# p working_copy_name = original_name.downcase.split('  ')
-# .  use the .split or .chars method to break apart string into 2 items in array
-
-# p working_copy_name = working_copy_name.reverse
- working_copy_name = original_name.downcase.split('')
-
-# 2. create a vowel data structure containing replacement vowels for new spy name
-#  --> within the next_vowel method
-# for each letter, see if the character matches the vowel hash
-  # if it does, look at the next key (#map or #each)
-  # if not, leave the letter alone. 
-
-  # collection is an array
-def next_vowel(collection)
-  vowel_hash = { a: "e", e: "i", i: "o", o: "u", u: "a" }
-
-  collection.map do |letter|
-    vowel_hash[letter.to_sym] || letter 
-  end
-  
-end 
- 
- next_vowel(working_copy_name)
-
-
-
-#  pool of consonants to create new spy name
-def next_consonant(array_version)
-  consonant_hash = {b: "c", c: "d", d: "f", f: "g", g: "h", h: "j", j: "k", k: "l", l: "m", m: "n", n: "p", p: "q", q: "r", r: "s", s: "t", t: "v", v: "w", w: "x", x: "y", y: "z", z: "b" }
-
-  array_version.map do |letter|
-    consonant_hash[letter.to_sym] || letter
-  
-  end
-
-
-end
-
-
-# use .join method to put characters together again
-
-nexted_letters =  next_consonant(next_vowel(working_copy_name)).join
-
-def name_to_array (variable)
-  variable.split(' ').reverse
-end
-
-# spy_name_order = name_to_array(nexted_letters)
-
-
-capital_alias_as_array = name_to_array(nexted_letters).map { |first_letter| first_letter.capitalize }
-# final_alias = spy_name_order.map(&:capitalize).join(' ')
-p final_alias = capital_alias_as_array.join(' ')
 
  
-  
-
-
-  
-
-
-# if other character like a space or - or something, keep the same.
-# . use a .replace_if method
-
-
-
-
-
-
+original_name = nil
+until original_name == "quit"
+  # 1. allows user to enter a name and have it stored as a variable temporarily
+    puts "enter original name or 'quit' to stop"
+      original_name = gets.chomp
+    
+    
+    # . use .downcase method to make all characters lower case
+    # p working_copy_name = original_name.downcase.split('  ')
+    # .  use the .split or .chars method to break apart string into 2 items in array
+    
+    # p working_copy_name = working_copy_name.reverse
+     working_copy_name = original_name.downcase.split('')
+    
+    # 2. create a vowel data structure containing replacement vowels for new spy name
+    #  --> within the next_vowel method
+    # for each letter, see if the character matches the vowel hash
+      # if it does, look at the next key (#map or #each)
+      # if not, leave the letter alone. 
+    
+      # collection is an array
+    def next_vowel(collection)
+      vowel_hash = { a: "e", e: "i", i: "o", o: "u", u: "a" }
+    
+      collection.map do |letter|
+        vowel_hash[letter.to_sym] || letter 
+      end
+      
+    end 
+     
+     next_vowel(working_copy_name)
+    
+    
+    
+    #  pool of consonants to create new spy name
+    def next_consonant(array_version)
+      consonant_hash = {b: "c", c: "d", d: "f", f: "g", g: "h", h: "j", j: "k", k: "l", l: "m", m: "n", n: "p", p: "q", q: "r", r: "s", s: "t", t: "v", v: "w", w: "x", x: "y", y: "z", z: "b" }
+    
+      array_version.map do |letter|
+        consonant_hash[letter.to_sym] || letter
+      
+      end
+    
+    
+    end
+    
+    
+    # use .join method to put characters together again
+    
+    nexted_letters =  next_consonant(next_vowel(working_copy_name)).join
+    
+    def name_to_array (variable)
+      variable.split(' ').reverse
+    end
+    
+    # spy_name_order = name_to_array(nexted_letters)
+    
+    
+    capital_alias_as_array = name_to_array(nexted_letters).map { |first_letter| first_letter.capitalize }
+    # final_alias = spy_name_order.map(&:capitalize).join(' ')
+    p final_alias = capital_alias_as_array.join(' ')
+end
 
 
 
