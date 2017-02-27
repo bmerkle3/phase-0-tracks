ef46e42acb4d398aef1f7feb01cd60148b8733cc
bmerkle3 <braidymerkle@gmail.com>
2017-02-27 11:44:56 -0600

    completes release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,52 +1,93 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+
+# end
+
+
+# class Puppy
+#  def initialize
+#     print "In itializing new puppy instance ..."
+#  end
+
+#  def fetch(toy)
+#    puts "I brought back the #{toy}!"
+#    toy
+#  end
+
+#  def speak(num)
+#    puts 'Woof!' * num
+#  end
+
+#  def roll_over
+#    print "*rolls over*" + "\n"
+#  end
+
+#  def dog_years(human_years)
+#    print human_years * 7
+#  end
+
+#  def add_trick(trick)
+#    if trick == 'waits'
+#     puts 'Dog is staying'
+#    elsif trick == 'sit'
+#     puts 'Dog is sitting'
+#   else
+#    puts 'Good dog'
+#  end
+
+#  end
+# end
+
+
+# teddy = Puppy.new
+# teddy.fetch('ball')
+# teddy.speak(4)
+# teddy.roll_over
+# teddy.dog_years(4)
+# teddy.add_trick('bark')
+# teddy.initialize
+
+
+class Cat
+
+  def initialize
+  print "Initializing new cat instance..."
+  end
+  
+  def speak(num)
+    puts "Purrr" * num
+  end
+  
+  
+  def chase(animal)
+    puts "I just chased a #{animal}!"
   end
 
 end
 
-katyvonkaty [10:41 AM] 
-class Puppy
- def initialize
-    print "In itializing new puppy instance ..."
- end
 
- def fetch(toy)
-   puts "I brought back the #{toy}!"
-   toy
- end
 
- def speak(num)
-   puts 'Woof!' * num
- end
+felix = Cat.new
+cats = []
 
- def roll_over
-   print "*rolls over*" + "\n"
- end
+50.times do 
+  cats <<  felix
+end
+puts cats.size
 
- def dog_years(human_years)
-   print human_years * 7
- end
+store_cats = []
 
- def add_trick(trick)
-   if trick == 'waits'
-    puts 'Dog is staying'
-   elsif trick == 'sit'
-    puts 'Dog is sitting'
-  else
-   puts 'Good dog'
- end
+p cats
 
- end
+cats.each do |x|
+ puts x.speak(2) 
 end
 
+cats.each do |x|
+  puts x.chase("mouse")
+end
 
-teddy = Puppy.new
-teddy.fetch('ball')
-teddy.speak(4)
-teddy.roll_over
-teddy.dog_years(4)
-teddy.add_trick('bark')
-teddy.initialize
