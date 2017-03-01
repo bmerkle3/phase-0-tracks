d5a012890870306db36f2fec0d8a0fe60a8e64b1
bmerkle3 <braidymerkle@gmail.com>
2017-02-28 20:41:26 -0600

    copies week5_test into ruby folder

---
+++ ruby/week5_test_copy.rb
@@ -0,0 +1,50 @@
+
+# addition_input = []
+# addition_problem = nil
+
+# until addition_problem == "done"
+#   puts "enter an addition problem (use spaces between values)"
+#     addition_problem = gets.chomp 
+#     addition_input = addition_problem.split(' ')
+     
+  
+  
+#   def calculate(integer1, integer2)
+#     integer1 + integer2
+
+#   end
+  
+  
+#   p solution = calculate(addition_input[0].to_i,  addition_input[-1].to_i)
+
+# solutions_database = {}
+
+
+# end 
+
+# solutions_database [solution] = addition_problem
+# puts  "number of calculations performed:"
+# puts solutions_database.length
+  
+#   solutions_database.each do |i1, i2|
+#     p "#{i1} + #{i2} is" 
+ 
+#   end
+
+
+# def calculate(a, op, b)
+#    a.send(op b) 
+# end
+
+# p calculate(2, ":+" , 3)
+
+
+operations = {:+ => "+", :- => "-", :* => "*", :/ => "/"}
+def add(i1, op, i3)
+  i1 op i2  
+end
+
+
+p add( 1, :+, 3)
+
+# https://devbootcamp.instructure.com/courses/143/quizzes/2103?module_item_id=40813
