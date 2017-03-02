07494bc8ac30ae5dbb6e67732a8ce1ba00c3e5b3
bmerkle3 <braidymerkle@gmail.com>
2017-03-02 14:20:53 -0600

    completes release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,7 @@
 class Santa
+  attr_reader :ethnicity, :age 
+  attr_accessor :gender
+
   def initialize (gender, ethnicity)
     @gender = gender
     @ethnicity  = ethnicity
@@ -14,16 +17,20 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
+  def randomize_age(i)
+    age = rand(i)
+  end 
+
 #Had to add this method to get the gender to print: 
 #the @ethnicity only works within this class initialization. To call from 
 #outside, a method is needed. 
-  def states_gender
-    puts "This santa is #{@gender}"
-  end
+  # def states_gender
+  #   puts "This santa is #{@gender}"
+  # end
 
-  def states_ethnicity
-    puts "This santa is #{@ethnicity}"
-  end
+  # def states_ethnicity
+  #   puts "This santa is #{@ethnicity}"
+  # end
 
   def celebrate_birthday
     @age += 1
@@ -39,18 +46,18 @@ class Santa
 
 
   
-  #getter methods
-  def age
-    @age
-  end
+  #getter methods... all commented out for refactoring at end of release 3
+  # def age
+  #   @age
+  # end
 
-  def ethnicity
-    @ethnicity
-  end
+  # def ethnicity
+  #   @ethnicity
+  # end
 
-  def gender= (new_gender)
-    @gender = new_gender
-  end  
+  # def gender= (new_gender)
+  #   @gender = new_gender
+  # end  
 
 end
 #####
@@ -77,27 +84,28 @@ end
 #   puts "---------------------"
 # end
 
-# refractored version below
+# refactored version 
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
+#QUESTION:why can't I use #each method?
+example_genders.length.times do |i| #why not with .each???
   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
 
 
 st_nikita = Santa.new("agender", "Japanese-African")
 # st_nikita.states_gender
-st_nikita.states_ethnicity
+# st_nikita.states_ethnicity
 
-#QUESTION: how to get this to print ethnicity and gender (without a getter or setter)?? ANSWER: pass in the 
+#(was q): how to get this to print ethnicity and gender (without a getter or setter)?? ANSWER: pass in the 
 #object you want the instance method to be called on (add the "santa" parameter)
-santas.each do |santa|
-  santa.states_gender
-  santa.states_ethnicity
-  puts "There are now #{santas.length} santa instances in the array" 
-  puts "---------------------"
-end
+  # santas.each do |santa|
+  #   santa.states_gender
+  #   santa.states_ethnicity
+  #   puts "There are now #{santas.length} santa instances in the array" 
+  #   puts "---------------------"
+  # end
 
 # driver code checking get_mad_at, age, celebrate_birthday, setter method for gender
 st_nikita.get_mad_at("Rudolph")
@@ -106,6 +114,36 @@ p st_nikita.celebrate_birthday
 p st_nikita.age
 p st_nikita.ethnicity
 p st_nikita.gender = "bigender"
+
+
+###############################################################################
+###release 4#####
+###############################################################################
+p "release4 starts here"
+
+santas1 = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+98.times do |i|
+  santas1 << Santa.new(example_genders.sample, example_ethnicities.sample)
+  #age = rand(140) #<-- have to add new instance method to randomize age?
+end
+
+vicky = santas1[42]
+p vicky
+p vicky.randomize_age(140)
+p vicky.ethnicity
+p vicky.gender
+
+#get program to loop about 100 times
+#generate random genders paired with random ethnicity
+  #similar to block above, where index 'i' is variable set to random number
+  # use arr.sample method
+
+#age to random number between 1-140
+
+
+
 
 
 
