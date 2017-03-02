006a023a70239562de2c675f1cd79ece84c97ae9
bmerkle3 <braidymerkle@gmail.com>
2017-03-02 15:20:54 -0600

    adds shout method to two new classes, completes release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,39 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + "! OMG!!!" " :)"
+#   end
+
+# p self.yell_angrily("heyy")
+# p self.yelling_happily("wow")
+# end
+
 module Shout
-  #something...
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+    def yelling_happily(words)
+    words + "! OMG!!!" " :)"
+  end
+ end 
+
+
+class Angry_parent
+  include Shout
 end
 
+class Sports_fan
+  include Shout
+end
+
+parent = Angry_parent.new 
+p parent.yell_angrily("Go to your room")
+p parent.yelling_happily("I love you so much")
+
+fan = Sports_fan.new
+p fan.yelling_happily("What a play")
+p fan.yell_angrily("Are you blind, ref?? What a terrible call")
