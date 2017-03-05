b4cd9f6c39fd112505a2a352964c59949d759c60
bmerkle3 <braidymerkle@gmail.com>
2017-03-04 20:21:06 -0600

    writes 1 test out of 3. fails as expected

--- ruby/game_spec.rb
+++ ruby/game_spec.rb
@@ -1,1 +1,14 @@
 require_relative 'game'
+
+
+describe Word_game do
+  let (:game) { Word_game.new }
+
+  it "changes characters to underscores" do
+    expect(game.char_to_underscore("word1")).to eq "_____"
+  end
+
+
+
+  
+end
