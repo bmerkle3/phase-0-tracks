629db5853b9be911fef1ce3a1c2838622744b33a
bmerkle3 <braidymerkle@gmail.com>
2017-03-13 01:02:39 -0500

    adds release 2 solution minus driver code

--- js/algos.js
+++ js/algos.js
@@ -34,56 +34,174 @@ function printLongest(anyStringArray) {
  }
 /////////////////////////////////////////////////////////////////////////////////////
 //release 1
+// creates key arrays
 // input is two objects
+// change objects to 2 arrays of keys 
+// save arrays in method
+
+// creates value arrays
+// input is two objects
+ // 2 arrays of values
+ // saves arrays in method
 // 
+// check key arrays with key arrays
+// input is 2 arrays
 // start with match as false
 // for each key in objectA, check to see if it matches a key in objectB
   // if match is found, returns true (set to variable for match found = true)
-  // flag to break loop if match is true
   // if no match is found, returns false
 
-// input is two objects
+// Check value arrays with value arrays
+// input is two arrays
 // start with match as false
 // for each value in objectA, check to see if it matches a value in objectB
-  // if match his found, returns true (set to variable for match found = true)
+  // if match is found, returns true (set to variable for match found = true)
     // flag to break loop if match is true
   // if no match is found, returns false
 
+// if key-match and value-match are both true, returns true
+// otherwise, returns false
 
-// if key match and valuematch are false, returns false
-// otherwise, returns true
-var myVacations = {"Hawaii", "Alaska", "Iceland", "China"}
-var friendVacations = {"Bali", "Iceland", "Russia", "Austria"}
-var sisterVacations = {"Italy", "Mexico", "Louisiana", "Washington DC"}
+var myVacations = {"Hawaii" : 2 , "Alaska" : 1, "Iceland" : 4, "China" : 0}
+var friendVacations = {"Bali" : 1, "Iceland" :1, "Russia" : 2, "Austria" : 1}
+var sisterVacations = {"Italy" : 1, "Mexico" : 6, "Louisiana" : 6, "Washington DC" : 5}
+var obj1Keys = [];
+var obj2Keys = [];
+
+var obj1Values = [];
+var obj2Values = []
+
+function keysToArray(obj1, obj2) {
+  for (var key in obj1) {
+    obj1Keys.push(key);
+  }
+  for (var key in obj2) {
+    obj2Keys.push(key);
+  }
+ 
+   checksValue(keyChecker(obj1Keys, obj2Keys))
+}
+
+// function keysToArray(obj1) {
+//   for (var key in obj1) {
+//     obj1Keys.push(key);
+//   }
+// }
+
+// function valuesToArray(obj1, obj2) {
+//   for(var value in obj1) {
+//     obj1Values.push(obj1[value]);
+//   }
+//   for(var value in obj2) {
+//     obj2Values.push(obj2[value]);
+//   }
+
+// }
 
 
-function keyChecker(object1, object2) {
-  var matches = false
-  while(var i=0; i <object1.length; i++) {
-
-    
+function keyChecker(arr1, arr2) {
+  var keyMatches = false
+  for(var i=0; i < arr1.length; i++) {
+    for(var j=0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+         console.log(arr1[i]);
+         keyMatches = true;
+         return (i)
       }
+    }
   }
 }
 
+function checksValue(i) {
+  // if obj1[i] === obj2(obj1[i])
+}
+10, 8, 7, 6, 5, 4, 2, 1
+
+// function valueChecker(object1, object2) {
+//   var valueMatches = false
+//   for(var i=0; i < object1.length; i++) {
+//     for(var j=0; j < object2.length; i++) {
+//       if (object1[i] === object2[j]) {
+//         valueMatches = true;
+//         return object1[i];
+//       }
+//     }
+    
+//       }
+//   }
+
+
+
 // ////////////////////////////////////////////////////////////////////////////
-/* input is integer.
-// uses hashes so keys are 0-(i-1)?
-// for each key: 
+/* 
+
+no input
 // size variable will be random number 1 to 10 to determine size of word.
-// Math.floor(Math.random() * 10)
-// loop if num = 0
-// add random letters size variable number of times until variable length equals the integer input. 
+// add random letters (size variable number of times) until variable length equals the random integer. 
+
+input is integer.
+//loops through the process input integer number of times
 */
+
+
+function makesWord() {
+  var lettersBank = "abcdefghijklmnopqrstuvwxyz";
+  var word = ""
+  // var array = []
+  for( var i = 0; i <= (Math.floor(Math.random() * 10) + 1); i++) 
+    word += lettersBank.charAt(Math.floor(Math.random() * lettersBank.length))
+  return word
+
+}
+
+
+function arraysOfWords(i) {
+ var array = [];
+ for (j = 1 ; j < i+1; j++) 
+  array.push(makesWord())
+  return array;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  // Driver Code /////////////
 //for release 0:
-var originalArray = ["long phrase", "a", "longest phrase"]
-printLongest(originalArray)
+// var originalArray = ["long phrase", "a", "longest phrase"]
+// printLongest(originalArray)
 
-var array1 = ["b", "this is the longest phrase", "sssssss"]
-printLongest(array1)
-
-
+// var array1 = ["b", "this is the longest phrase", "sssssss"]
+// printLongest(array1)
 
 
+
+// // for release 1
+// // console.log(myVacations)
+// console.log(keysToArray(myVacations, friendVacations))
+// console.log(obj1Keys)
+// console.log(obj2Keys)
+
+// console.log(valuesToArray(myVacations, friendVacations))
+// console.log(obj1Values)
+// console.log(obj2Values)
+
+// // console.log(keyChecker(obj1Keys, obj2Keys))
+
+// console.log(makesWord())
+console.log(arraysOfWords(6))
+
+// for (i = 0 ; )
