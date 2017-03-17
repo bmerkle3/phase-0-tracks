c08d594c445579719fdccee330e7c163b88db1a5
bmerkle3 <braidymerkle@gmail.com>
2017-03-17 15:33:42 -0500

    adds two tables : student and problem

--- week8_proj/flashcards.rb
+++ week8_proj/flashcards.rb
@@ -6,7 +6,7 @@ db = SQLite3::Database.new( "mathflashcard.db" )
 
 #delimiters
 create_student_cmd = <<-SQLite3
-  CREATE TABLE student(
+  CREATE TABLE IF NOT EXISTS student(
     id INTEGER PRIMARY KEY, 
     name VARCHAR(255)
   );
@@ -14,16 +14,16 @@ create_student_cmd = <<-SQLite3
 SQLite3
 
 create_problem_cmd = <<-SQLite3
-  CREATE TABLE problem(
+  CREATE TABLE IF NOT EXISTS problem(
     id INTEGER PRIMARY KEY, 
     individ_problem VARCHAR(255),
-    problem_set INTEGER)
+    problem_set INTEGER
   );
 
 SQLite3
 
 create_students_problems_cmd = <<-SQLite3
-  CREATE TABLE students_problems(
+  CREATE TABLE IF NOT EXISTS students_problems(
     id INTEGER PRIMARY KEY, 
     answered_incorrect INTEGER, 
     answered_correct INTEGER,
@@ -38,6 +38,7 @@ SQLite3
 #create student table (if not already there)
 db.execute(create_student_cmd)
 
-# db.excecute(create_problem_cmd)
+db.execute(create_problem_cmd)
 
-# db.excecute(create_students_problems_cmd)
+# db.execute(create_students_problems_cmd)
+
