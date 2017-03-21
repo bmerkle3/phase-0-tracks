675413b20764cdf7db6e90da73145c9ab39a624d
bmerkle3 <braidymerkle@gmail.com>
2017-03-21 13:34:03 -0500

    makes picture of pizza dissappear when h1 is clicked

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,17 +1,17 @@
 console.log("Script is running!")
 
-function hidePizzaButton(event) {
- event.target.hidden = true
-  // pic.hidden = true;
+function hidePizzaButton() {
+  var el = document.getElementById("button")
+  pic.hidden = true;
 }
 
 
 
 
-// function hiddenPizza() {
-//   var pic = document.getElementById("pizza");
-//   pic.hidden = true;
-// }
+function hiddenPizza() {
+  var pic = document.getElementById("pizza");
+  pic.hidden = true;
+}
 
 var pic = document.getElementById("pizza");
 var el = document.getElementById("button");
