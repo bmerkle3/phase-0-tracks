747f26bff06bd1dc06cbc5a16e65d2b5eb1c8271
bmerkle3 <braidymerkle@gmail.com>
2017-03-26 01:41:35 -0500

    adds 4 htmlparagraphs, button, img and jquery file that responds

---
+++ web_dev/jquery/script_for_jq.js
@@ -0,0 +1,20 @@
+// window.onload = function() {
+//      alert( "welcome" );
+//  }
+
+
+$('p:eq(2)').css('border', '4px solid red');
+
+$('button').slideUp(500).show(800);
+
+// $('#dis').fadeOut(800);
+
+$(document).ready(function() {
+  $('button').click(function() {
+    $('#dis').fadeOut(800);
+  });
+});
+
+$('img').delay(1900).fadeOut(500, function() {
+    $(this).attr('src', 'images/green_tracks.jpeg').fadeIn(700);
+});
